f4b385b5c33bd2ce39d07f26a527d99f36df2e80
ParticleGeorge <georgek.sanchez@outlook.com>
2025-03-04 15:33:20 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main (){
+    // rudimentary framework
+    std::string firstName, lastName;
+    
+    std::cout << "Welcome to CSUSB Student Registration\n" << "Please make a selection\n";
+    // placeholder for menu
+    // begin add function
+    // veryyyyyyy basic 
+    
+    std::cout << "Please enter First Name:\n";
+    std::cin >> firstName;
+    std::cout << "Please enter Last Name:\n";
+    std::cin >> lastName;
+
+    std::cout << firstName << " " << lastName << std::endl;
+
+    return 0; 
+
+}
+

25ffc2a8ede80deca221e0d51678a9804ead959a
Zeratul4340 <eduardoatroom37@gmail.com>
2025-03-04 18:29:15 -0800

    testing gitlens on VScode

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 int main (){
     // rudimentary framework
+    // testing gitlens of VScode, will remove this comment
     std::string firstName, lastName;
     
     std::cout << "Welcome to CSUSB Student Registration\n" << "Please make a selection\n";
